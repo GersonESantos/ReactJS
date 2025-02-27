0e1182c71a0e604f355a060ed528c437aa632dcb
GersonESS <gebhsantos@gmail.com>
2025-02-27 10:24:30 -0300

    a

--- vite-project01/src/index.js
+++
@@ -1,8 +0,0 @@
-// Importações
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-// Estrutura do render
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-root.render(<h1>Meu primeiro render</h1>);
