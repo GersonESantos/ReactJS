4ced4499223305a160c8fef950fb301c6230d6b7
GersonESS <gebhsantos@gmail.com>
2025-02-27 13:25:32 -0300

    a

---
+++ 05 - JSX/05/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const elemento = React.createElement('h1',{},'Sem JSX');
+ReactDOM.render(
+    elemento,
+    document.getElementById('root')
+);
+
