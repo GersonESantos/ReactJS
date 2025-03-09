041768465c23e0a529f22adba8cf460cc0407bb7
GersonESS <gebhsantos@gmail.com>
2025-03-09 18:34:37 -0300

    Update index.tsx

--- yt02/startapp/src/app/index.tsx
+++ yt02/startapp/src/app/index.tsx
@@ -1,35 +1,27 @@
 import { View, Text, StyleSheet, Alert, Button } from "react-native";
 
 export default function Index() {
-    const handleMessage = () => {
-        Alert.alert(
-            "Teste",
-            "Hello GersonES!",
-            [
-                { text: "OK", onPress: () => console.log("OK Pressed") }
-            ]
-        );
-    };
-
+    function handleMessage() {
+        Alert.alert("Hello GersonES!")
+    }
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Ola GersonES!</Text>
-            {/* console.log("Botão clicado");  */}
-            <Button title="Mensagem" onPress={handleMessage} />
+        <Text style={styles.title}>Ola GersonES!</Text>
+        <Button title="Mensagem" onPress={handleMessage} />
         </View>
     );
-}
+    }
 
 const styles = StyleSheet.create({
     container: { 
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        padding: 32    
-    },
-    title: { 
-        color: "red",
+         padding: 32    
+        },
+    title :{ 
+        color: "red", 
         fontSize: 24,
-        fontWeight: "bold"
-    }
-});
+        fontWeight: "bold",
+    },
+    });
