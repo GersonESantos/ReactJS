72ac1a765e51a0aaa37f7850465dfcdebc663f2d
GersonESS <gebhsantos@gmail.com>
2025-03-09 17:55:30 -0300

    a

---
+++ yt02/startapp/src/components/button/index.tsx
@@ -0,0 +1,8 @@
+import { TouchableOpacity, Text } from 'react-native';
+export function Button() {
+    return (
+        <TouchableOpacity>
+            <Text>Clique</Text>
+        </TouchableOpacity>
+    )
+}
